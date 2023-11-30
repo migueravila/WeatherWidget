a2ec8125b9c3af096a5249555a5c583b2d476166
Miguel Avila <me@avila.sh>
2023-11-30 17:48:35 -0600

    refactor(index): Replace fetch with weatherService

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,36 @@
+//Imports
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../styles/globals.scss';
 
+//Components
 import WeatherCard from '../components/WeatherCard';
 
+//Services
+import getWeatherData from '../services/weatherService';
+
+//Main Index Component
 const Home: React.FC = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState<string | null>(null);
   const [location, setLocation] = useState('Monterrey');
 
-  const getWeatherData = async (city: string) => {
-	try {
-	  const response = await axios.get(
-		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`
-	  );
-	  
-	  console.log(response);
-
-	  if (response.data.cod !== 200) {
-		setError(response.data.message);
-		setWeatherData(null);
-		return;
-	  }
-	  
-	  setError(null);
-
-	  setWeatherData({
-		city: response.data.name,
-		temperature: response.data.main.temp,
-		temperatureMax: response.data.main.temp_max,
-		temperatureMin: response.data.main.temp_min,
-		windSpeed: response.data.wind.speed,
-		humidity: response.data.main.humidity,
-		description: response.data.weather[0].description,
-		iconCode: response.data.weather[0].icon,
-	  });
-	  
-	} catch (error) {
-	  setError("Unrecognized city");
-	  setWeatherData(null);
-	}
-  };
-	
-  useEffect(() => {
-	  getWeatherData(location);
-	}, [location]); // Re-fetch when the location changes
+  const fetchData = async (city: string) => {
+	  const { data, error } = await getWeatherData(city);
   
+	  if (error) {
+		setError(error);
+		setWeatherData(null);
+	  } else {
+		setError(null);
+		setWeatherData(data);
+	  }
+	};
+  
+	useEffect(() => {
+	  fetchData(location);
+	}, [location]); 
+	
 	return (
 	  <div className="flex items-center justify-center h-screen">
 		<WeatherCard
